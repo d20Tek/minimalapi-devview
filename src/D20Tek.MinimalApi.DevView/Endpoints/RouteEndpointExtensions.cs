fd24b49f89fa7c92665e6cc28d5e00c872b00fcf
Pedro Silva <DarthPedro@gmail.com>
2025-05-21 15:37:43 -0700

    Refactored data retrieval into specific extension methods.

--- src/D20Tek.MinimalApi.DevView/Endpoints/RouteEndpointExtensions.cs
+++ src/D20Tek.MinimalApi.DevView/Endpoints/RouteEndpointExtensions.cs
@@ -6,44 +6,58 @@ namespace D20Tek.MinimalApi.DevView.Endpoints;
 
 internal static class RouteEndpointExtensions
 {
+    private const string _noData = "none";
+
     public static Dictionary<string, object?> InspectEndpoint(this RouteEndpoint endpoint, DevViewOptions options)
     {
-        var routePattern = endpoint.RoutePattern.RawText ?? "N/A";
+        var routePattern = endpoint.GetRoutePattern() ?? _noData;
         if (routePattern.StartsWith(options.BasePath)) return [];
 
         var routeInfo = new Dictionary<string, object?>
         {
-            ["Method"] = endpoint.Metadata.OfType<HttpMethodMetadata>().FirstOrDefault()?
-                                          .HttpMethods.FirstOrDefault() ?? "N/A",
+            ["Method"] = endpoint.GetHttpMethods(),
             ["Pattern"] = routePattern
         };
 
         if (options.IncludeRouteMetadata)
         {
             routeInfo["Handler"] = endpoint.RequestDelegate?.Method?.Name;
+            routeInfo["Name"] = endpoint.GetEndpointName();
 
             if (GetProducesResponses(endpoint) is { Length: > 0 } produces)
             {
                 routeInfo["Produces"] = produces;
             }
 
-            if (options.IncludeRouteDebugDetails)
-            {
-                routeInfo["MetadataTypes"] = endpoint.Metadata.Select(t => $"Type: {t.GetType().Name}, Data:{t}");
-            }
+        }
+
+        if (options.IncludeRouteDebugDetails)
+        {
+            routeInfo["MetadataTypes"] = endpoint.Metadata.Select(t => $"Type: {t.GetType().Name}, Data:{t}");
         }
 
         return routeInfo;
     }
 
+    private static string? GetRoutePattern(this RouteEndpoint endpoint) => endpoint.RoutePattern.RawText;
+
+    private static string[] GetHttpMethods(this RouteEndpoint endpoint) =>
+        endpoint.Metadata.OfType<HttpMethodMetadata>()
+                         .SelectMany(m => m.HttpMethods)
+                         .DefaultIfEmpty(_noData)
+                         .ToArray();
+
+    private static string? GetEndpointName(this RouteEndpoint endpoint) =>
+        endpoint.Metadata.OfType<EndpointNameMetadata>()
+                         .FirstOrDefault()?
+                         .EndpointName;
+
     private static string[] GetProducesResponses(RouteEndpoint endpoint) =>
         endpoint.Metadata.OfType<ProducesAttribute>()
-                         .Select(attr => attr.ToString() ?? "")
+                         .Select(attr => attr.ToString() ?? _noData)
                          .Concat(endpoint.Metadata
                                          .OfType<ProducesResponseTypeMetadata>()
                                          .Select(meta => meta.ToString()))
                          .Distinct()
                          .ToArray();
-
-    internal record MetadataSerialized(string Type, string Data);
 }
